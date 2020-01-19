055a36232654e02f7e67711afb14af2fbb3fd7d5
Ian Christopher B. de Jesus <ian.dejesus.md@gmail.com>
2020-01-20 05:44:08 +0900

    add all redux exercises

---
+++ reducer-subscribe.js
@@ -0,0 +1,29 @@
+const ADD = "ADD";
+
+const reducer = (state = 0, action) => {
+  switch (action.type) {
+    case ADD:
+      return state + 1;
+    default:
+      return state;
+  }
+};
+
+const store = Redux.createStore(reducer);
+
+// global count variable:
+let count = 0;
+
+// change code below this line
+const addOne = () => (count += 1);
+store.subscribe(addOne);
+// change code above this line
+
+store.dispatch({ type: ADD });
+console.log(count);
+store.dispatch({ type: ADD });
+console.log(count);
+store.dispatch({ type: ADD });
+console.log(count);
+
+//subscribe method adds one to count every time dispatch is called
